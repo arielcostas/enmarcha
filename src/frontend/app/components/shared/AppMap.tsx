faf0abcc42cb32a8ebbb90a2acedd74b772a9e51
Ariel Costas Guerrero <ariel@costas.dev>
2025-12-24 20:00:47 +0100

    Screw this

--- src/frontend/app/components/shared/AppMap.tsx
+++ src/frontend/app/components/shared/AppMap.tsx
@@ -195,10 +195,6 @@ export const AppMap = forwardRef<MapRef, AppMapProps>(
         onRotateStart={onRotateStart}
         onPitchStart={onPitchStart}
         onLoad={onLoad}
-        dragPan={{
-          linearity: 0.4,
-          deceleration: 3000,
-        }}
       >
         {showNavigation && <NavigationControl position="bottom-right" />}
         {showGeolocate && (
