11d30dae300a03af8659ae05a204cd2107b4983a
Ariel Costas Guerrero <ariel@costas.dev>
2026-06-05 10:24:57 +0200

    a

--- src/Enmarcha.Sources.OpenTripPlannerGql/Queries/RouteDetailsContent.cs
+++ src/Enmarcha.Sources.OpenTripPlannerGql/Queries/RouteDetailsContent.cs
@@ -112,7 +112,7 @@ public class RouteDetailsResponse : AbstractGraphResponse
     {
         [JsonPropertyName("scheduledArrival")] public int? ScheduledArrival { get; set; }
         [JsonPropertyName("scheduledDeparture")] public int? ScheduledDeparture { get; set; }
-        public int? ScheduledAt => ScheduledDeparture ?? ScheduledArrival;
+        public int? ScheduledAt => ScheduledDeparture ?? ScheduledArrival ?? -1;
         [JsonPropertyName("pickupType")] public string? PickupType { get; set; }
         [JsonPropertyName("dropoffType")] public string? DropoffType { get; set; }
     }
